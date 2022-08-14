9919a47fe56ae926dc320a5decc89773ef8833d2
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-08-14 17:21:15 +0200

    ListView delle ricette

--- lib/pages/category_meals_screen.dart
+++ lib/pages/category_meals_screen.dart
@@ -1,3 +1,4 @@
+import 'package:delimeals/dummy_data%20.dart';
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
@@ -13,11 +14,17 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] ?? 'Categoria';
+    final categoryId = routeArgs['id'] ?? 'c1';
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle)),
-      body: Center(
-        child: Text("Ricette per la categoria  $categoryTitle"),
-      ),
+      body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length),
     );
   }
 }
