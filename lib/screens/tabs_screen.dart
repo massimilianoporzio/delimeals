290609080a114816ae60f473b38aba181af82ad5
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-08-25 19:25:36 +0200

    aggiunto tabs screen vuoto

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tab extends StatefulWidget {
+  const Tab({Key? key}) : super(key: key);
+
+  @override
+  State<Tab> createState() => _TabState();
+}
+
+class _TabState extends State<Tab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
